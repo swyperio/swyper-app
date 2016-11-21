c690c71b4a5fa91d090fb5fbc4baa9372438795d
veganafro <jmuhia80@gmail.com>
2016-11-21 17:03:03 -0500

    Initial Commit

---
+++ swyperio/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  swyperio
+//
+//  Created by Jeremia Muhia on 11/21/16.
+//  Copyright © 2016 NYU. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
