b91fffb176121ec5835dfa7ed143500b2e4c36d3
veganafro <jmuhia80@gmail.com>
2016-12-03 17:14:11 -0500

    remove Pods folder

--- Pods/Target Support Files/Pods-swyperio/Pods-swyperio-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_swyperio : NSObject
-@end
-@implementation PodsDummy_Pods_swyperio
-@end
--- Pods/Target Support Files/Pods-swyperioUITests/Pods-swyperioUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_swyperioUITests : NSObject
-@end
-@implementation PodsDummy_Pods_swyperioUITests
-@end
