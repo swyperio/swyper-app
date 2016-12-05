a76dcd588b151cb23e416f812872e4677ec5033f
veganafro <jmuhia80@gmail.com>
2016-12-05 12:53:58 -0500

    build sign in functionality

--- swyperio/LogInViewController.swift
+++ swyperio/LogInViewController.swift
@@ -57,7 +57,6 @@ class LogInViewController: UIViewController {
     }
     
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
