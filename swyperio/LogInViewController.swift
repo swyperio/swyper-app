82be7bc6ea13ee71bb8663d8980670d5aef4dc4f
veganafro <jmuhia80@gmail.com>
2016-12-05 13:37:02 -0500

    build create account functionality

--- swyperio/LogInViewController.swift
+++ swyperio/LogInViewController.swift
@@ -19,6 +19,7 @@ class LogInViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         if let user = FIRAuth.auth()?.currentUser {
+            
             self.signedIn(user)
         }
     }
@@ -35,20 +36,57 @@ class LogInViewController: UIViewController {
         
         guard email != "" && password != ""
         else {
+            
             let invalidEmailOrPasswordAlert = UIAlertController(title: "Invalid Email or Password", message: "The email or password you gave is invalid", preferredStyle: UIAlertControllerStyle.alert)
             invalidEmailOrPasswordAlert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
             self.present(invalidEmailOrPasswordAlert, animated: true, completion: nil)
+            
             return
         }
+        
         FIRAuth.auth()?.signIn(withEmail: email, password: password) { (user, error) in
             if let error = error {
+                
                 print("ERROR OCCURED AT SIGN IN", error.localizedDescription)
+                
                 let userDoesNotExist = UIAlertController(title: "Invalid Email or Password", message: "The email or password you gave is invalid", preferredStyle: UIAlertControllerStyle.alert)
                 userDoesNotExist.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
                 self.present(userDoesNotExist, animated: true, completion: nil)
+                
                 return
             }
             self.signedIn(user!)
+            self.performSegue(withIdentifier: "signInSegue", sender: nil)
+        }
+    }
+    
+    @IBAction func didTapSignUp(_ sender: AnyObject?) {
+    
+        let email: String = emailTextField.text!
+        let password: String = passwordTextField.text!
+        
+        guard email != "" && password != ""
+        else {
+            
+            let invalidEmailOrPasswordAlert = UIAlertController(title: "Invalid Email or Password", message: "The email or password you gave is invalid", preferredStyle: UIAlertControllerStyle.alert)
+            invalidEmailOrPasswordAlert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            self.present(invalidEmailOrPasswordAlert, animated: true, completion: nil)
+            
+            return
+        }
+        
+        FIRAuth.auth()?.createUser(withEmail: email, password: password) { (user, error) in
+            if let error = error {
+                
+                print("ERROR OCCURED AT SIGN UP", error.localizedDescription)
+                
+                let accountNotCreated = UIAlertController(title: "Invalid Email or Password", message: "The email or password you gave is invalid", preferredStyle: UIAlertControllerStyle.alert)
+                accountNotCreated.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+                self.present(accountNotCreated, animated: true, completion: nil)
+                
+                return
+            }
+            self.setDisplayName(user!)
             self.performSegue(withIdentifier: "signInSegue", sender: nil)
         }
     }
@@ -61,6 +99,21 @@ class LogInViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+    }
+    
+    func setDisplayName(_ user: FIRUser) {
+    
+        let changeRequest = user.profileChangeRequest()
+        changeRequest.displayName = user.email!.components(separatedBy: "@")[0]
+        
+        changeRequest.commitChanges() { (error) in
+            
+            if let error = error {
+                print("ERROR OCCURED AT SET DISPLAY NAME", error.localizedDescription)
+                return
+            }
+            self.signedIn(FIRAuth.auth()?.currentUser)
+        }
     }
     
     func signedIn(_ user: FIRUser?) {
