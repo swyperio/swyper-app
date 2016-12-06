5850c9273f97250ed78d77fc2865acfd5dade1b6
veganafro <jmuhia80@gmail.com>
2016-12-05 20:34:58 -0500

    build core UI

--- swyperio/CreateServiceViewController.swift
+++ swyperio/CreateServiceViewController.swift
@@ -8,21 +8,43 @@
 
 import UIKit
 
-class CreateServiceViewController: UIViewController {
+class CreateServiceViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var serviceDetailTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.serviceDetailTable.register(UITableViewCell.self, forCellReuseIdentifier: "tableViewCell")
+        serviceDetailTable.delegate = self
+        serviceDetailTable.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "nameCell", for: indexPath)
+        cell.textLabel?.text = "TEST"
+        // cell.contentView.addSubview(UITextView())
+        return cell
+    }
+    
+    // table view cells should include a section for full name and time of swipe
 
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
