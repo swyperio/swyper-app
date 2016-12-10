9063426c33e607615adeadbf2cd67863be2abde4
veganafro <jmuhia80@gmail.com>
2016-12-10 17:16:11 -0500

    configure local environment

--- swyperio/ProfileViewController.swift
+++ swyperio/ProfileViewController.swift
@@ -155,7 +155,7 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
     }
     
     //handle what happens after a photo is selected (updates the profilePicture, the firebase storage and database
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         print("reached imagePickerController func")
         
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
