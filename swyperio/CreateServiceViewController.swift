f53f9de32124b0f5123c9f69eee5c30f4a17f984
Paul Pelayo <pp1286@nyu.edu>
2016-12-10 17:24:45 -0500

    updates to Event class (added userID) and wrote uploadEvent function for firebase

--- swyperio/CreateServiceViewController.swift
+++ swyperio/CreateServiceViewController.swift
@@ -7,23 +7,33 @@
 //
 
 import UIKit
+import MapKit
+import FirebaseAuth
+import FirebaseDatabase
+import FirebaseStorage
 
 class CreateServiceViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate,
     // Note: A service requires the following:
     // String: Event name <-- only required if an event, otherwise defaults to the host user's name
     // CLLocation: The location of the event (we'll have a global map of string->CLLocation). Instantiate one via CLLocation(latitude: 40.729508, longitude: -73.997181).
     // Date: startTime
-        // If a user: let startTime = NSDate()
-        // If an event organiser: let startTime = some input from a datepicker
+    // If a user: let startTime = NSDate()
+    // If an event organiser: let startTime = some input from a datepicker
     // Date: endTime
-        // If a user: let endTime = NSDate(timeIntervalSinceReferenceDate: 3600.0) // Sets a default endTime of 1 person
-        // If an event organiser: let endTime = some input from a datepicker, or just show how of a duration in hours, calculate the endTime, and save that value
+    // If a user: let endTime = NSDate(timeIntervalSinceReferenceDate: 3600.0) // Sets a default endTime of 1 person
+    // If an event organiser: let endTime = some input from a datepicker, or just show how of a duration in hours, calculate the endTime, and save that value
     // int: maxReservations <-- some maximum number of people that can "reserve" a spot on the event
-
-    UINavigationControllerDelegate {
+    
+UINavigationControllerDelegate {
     // @IBOutlet weak var serviceDetailTable: UITableView!
+    
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var startTimeTextField: UITextField!
+    @IBOutlet weak var endTimeTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    var user = FIRAuth.auth()?.currentUser
+    
     
     let datePickerView: UIDatePicker = UIDatePicker()//= CGRect(x: UIScreen.main.bounds.midX - 150, y: UIScreen.main.bounds.midY - 150, width: 300, height: 300))
     
@@ -42,16 +52,32 @@ class CreateServiceViewController: UIViewController, UITextFieldDelegate, UIImag
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
+        var myUserID = user?.uid
+        
+        let testEvent = Event(
+            name: "Test name",
+            coordinate: CLLocationCoordinate2D(latitude: 40.729508, longitude: -73.997181),
+            startTime: NSDate(),
+            endTime: NSDate(timeIntervalSinceReferenceDate: 3600.0),
+            maxReservations: 5,
+            information: "Test description",
+            userID: myUserID!
+        )
+        uploadEvent(testEvent)
+        
+        
+        
         // Do any additional setup after loading the view.
         /*
-        self.serviceDetailTable.register(UITableViewCell.self, forCellReuseIdentifier: "nameCell")
-        
-        serviceDetailTable.delegate = self
-        serviceDetailTable.dataSource = self
-        
-        serviceDetailTable.setNeedsLayout()
-        serviceDetailTable.layoutIfNeeded()
+         self.serviceDetailTable.register(UITableViewCell.self, forCellReuseIdentifier: "nameCell")
+         
+         serviceDetailTable.delegate = self
+         serviceDetailTable.dataSource = self
+         
+         serviceDetailTable.setNeedsLayout()
+         serviceDetailTable.layoutIfNeeded()
          */
         dateTextField.delegate = self
         //let tapGesture = UITapGestureRecognizer(target: self, action: Selector(("tapOutsideDatePicker:")))
@@ -64,12 +90,41 @@ class CreateServiceViewController: UIViewController, UITextFieldDelegate, UIImag
         self.datePickerView.isHidden = true
         self.dateTextField.text = "\(self.datePickerView.date)"
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func didTapDoneButton(_ sender: AnyObject) {
+        print("tapping done button")
+    }
+    
+    //function for uploading an event to firebase
+    func uploadEvent(_ event: Event){
+        print("begin uploading event")
+        
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEE, dd MMM yyy hh:mm:ss +zzzz"
+        
+        let ref = FIRDatabase.database().reference()
+        
+        ref.child("events").child(event.uniqueID).setValue(["user_id": event.userID,
+                                                            "name": event.name,
+                                                            "latitude": event.coordinate.latitude,
+                                                            "longitude": event.coordinate.longitude,
+                                                            "start_time": dateFormatter.string(from: event.startTime as Date),
+                                                            "end_time": dateFormatter.string(from: event.endTime as Date),
+                                                            "max_reservations": event.maxReservations,
+                                                            "information": event.information,])
+        
+        print("end uploading event")
+    }
+    
+    func retrieveAllEvents(){
+        
+    }
     // MARK: - Navigation
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
